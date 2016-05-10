2da956e8a0273e2ce88ccd818b8c2be7d39aa62b
Dmitry Jemerov <yole@jetbrains.com>
2016-05-10 20:07:33 +0200

    refactor deserializer to simplify seed API

--- src/main/kotlin/Deserializer.kt
+++ src/main/kotlin/Deserializer.kt
@@ -3,6 +3,7 @@ package ru.yole.jkid
 import java.io.Reader
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
+import java.util.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.jvm.javaType
@@ -18,25 +19,28 @@ fun Type.asJavaClass(): Class<Any> = when(this) {
 }
 
 abstract class Seed(val reflectionCache: ReflectionCache,
-                    val parent: Seed? = null,
-                    val parentParameter: KParameter? = null)  {
+                    val onDone: (Any?) -> Unit = {}) {
 
     abstract fun setProperty(name: String, value: Any?)
     abstract fun seedForPropertyValue(propertyName: String): Seed
-    protected abstract fun plant(param: KParameter, value: Any?)
+    abstract fun done()
 
-    open fun plantIntoParent(): Seed {
-        parent!!.plant(parentParameter!!, spawn())
-        return parent
+    internal fun seedForType(paramType: Type, onDone: (Any?) -> Unit): Seed {
+        val paramClass = paramType.asJavaClass()
+
+        if (Collection::class.java.isAssignableFrom(paramClass)) {
+            val parameterizedType = paramType as? ParameterizedType ?:
+                    throw UnsupportedOperationException("Unsupported parameter type $this")
+
+            return CollectionSeed(parameterizedType.actualTypeArguments.single(), reflectionCache, onDone)
+        }
+        return ObjectSeed(paramClass.kotlin, reflectionCache, onDone)
     }
-
-    abstract fun spawn(): Any?
 }
 
 class ObjectSeed<out T: Any>(private val targetClass: KClass<T>,
                              reflectionCache: ReflectionCache,
-                             parent: Seed? = null,
-                             parentParameter: KParameter? = null) : Seed(reflectionCache, parent, parentParameter) {
+                             onDone: (Any?) -> Unit = {}) : Seed(reflectionCache, onDone) {
     private val constructor = targetClass.primaryConstructor
             ?: throw UnsupportedOperationException("Only classes with primary constructor can be deserialized")
     private val parameters = constructor.parameters
@@ -50,7 +54,7 @@ class ObjectSeed<out T: Any>(private val targetClass: KClass<T>,
     override fun seedForPropertyValue(propertyName: String): Seed {
         val param = reflectionCache[targetClass].findParameter(propertyName) ?: return DeadSeed(this)
 
-        return seedForType(this, param, param.type.javaType)
+        return seedForType(param.type.javaType) { value -> arguments[param] = value }
     }
 
     private fun deserializeValue(value: Any?, param: KParameter): Any? {
@@ -72,7 +76,7 @@ class ObjectSeed<out T: Any>(private val targetClass: KClass<T>,
 
     }
 
-    override fun spawn(): T {
+    fun spawn(): T {
         for (param in parameters) {
             if (arguments[param] == null && !param.isOptional && !param.type.isMarkedNullable) {
                 throw SchemaMismatchException("Missing value for parameter ${param.name}")
@@ -81,30 +85,14 @@ class ObjectSeed<out T: Any>(private val targetClass: KClass<T>,
         return constructor.callBy(arguments)
     }
 
-    override fun plant(param: KParameter, value: Any?) {
-        arguments[param] = value
-    }
-
-    companion object {
-        internal fun seedForType(parent: Seed, param: KParameter?, paramType: Type): Seed {
-            val paramClass = paramType.asJavaClass()
-
-            if (Collection::class.java.isAssignableFrom(paramClass)) {
-                val parameterizedType = paramType as? ParameterizedType ?:
-                        throw UnsupportedOperationException("Unsupported parameter type $this")
-
-                return CollectionSeed(parameterizedType.actualTypeArguments.single(), parent.reflectionCache, parent, param)
-            }
-            return ObjectSeed(paramClass.kotlin, parent.reflectionCache, parent, param)
-        }
-
+    override fun done() {
+        onDone(spawn())
     }
 }
 
 class CollectionSeed(val elementType: Type,
                      reflectionCache: ReflectionCache,
-                     parent: Seed,
-                     parentParameter: KParameter?) : Seed(reflectionCache, parent, parentParameter) {
+                     onDone: (Any?) -> Unit) : Seed(reflectionCache, onDone) {
 
     private val elements = mutableListOf<Any?>()
 
@@ -113,44 +101,42 @@ class CollectionSeed(val elementType: Type,
     }
 
     override fun seedForPropertyValue(propertyName: String): Seed {
-        return ObjectSeed.seedForType(this, parentParameter, elementType)
+        return seedForType(elementType) { value -> elements.add(value) }
     }
 
-    override fun spawn() = elements
-
-    override fun plant(param: KParameter, value: Any?) {
-        elements.add(value)
+    override fun done() {
+        onDone(elements)
     }
 }
 
-class DeadSeed(parent: Seed) : Seed(parent.reflectionCache, parent, null) {
+class DeadSeed(parent: Seed) : Seed(parent.reflectionCache, {}) {
     override fun setProperty(name: String, value: Any?) { }
     override fun seedForPropertyValue(propertyName: String): Seed = this
-    override fun plantIntoParent(): Seed = parent!!
-
-    override fun plant(param: KParameter, value: Any?) = throw UnsupportedOperationException()
-
-    override fun spawn(): Any? = throw UnsupportedOperationException()
+    override fun done() {}
 }
 
 fun <T: Any> deserialize(json: Reader, targetClass: KClass<T>): T {
     val seed = ObjectSeed(targetClass, ReflectionCache())
     var currentSeed: Seed = seed
+    val seedStack = Stack<Seed>()
+
     val callback = object : JsonParseCallback {
         override fun enterObject(propertyName: String) {
+            seedStack.push(currentSeed)
             currentSeed = currentSeed.seedForPropertyValue(propertyName)
         }
 
         override fun leaveObject() {
-            currentSeed = currentSeed.plantIntoParent()
+            currentSeed.done()
+            currentSeed = seedStack.pop()
         }
 
         override fun enterArray(propertyName: String) {
-            currentSeed = currentSeed.seedForPropertyValue(propertyName)
+            enterObject(propertyName)
         }
 
         override fun leaveArray() {
-            currentSeed = currentSeed.plantIntoParent()
+            leaveObject()
         }
 
         override fun visitProperty(propertyName: String, value: Token.ValueToken) {
