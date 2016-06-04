67ac465b70179200a43de6a29d4815183ba8687f
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-06-04 11:07:08 +0200

    Added test for exercise on 'DateFormat' annotation

---
+++ src/main/kotlin/exercise/DateFormat.kt
@@ -0,0 +1,4 @@
+package ru.yole.jkid
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class DateFormat(val format: String)
---
+++ src/test/kotlin/exercise/DateFormatAnnotation.kt
@@ -0,0 +1,29 @@
+package ru.yole.jkid.exercise
+
+import org.junit.Ignore
+import org.junit.Test
+import ru.yole.jkid.DateFormat
+import ru.yole.jkid.deserialization.deserialize
+import ru.yole.jkid.serialization.serialize
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.test.assertEquals
+
+data class Person(
+        val name: String,
+        @DateFormat("dd-mm-yyyy") val birthDate: Date
+)
+
+@Ignore
+class DateFormatTest {
+    private val value = Person("Alice", SimpleDateFormat("dd-mm-yyyy").parse("13-02-1987"))
+    private val json = """{"birthDate": "13-02-1987", "name": "Alice"}"""
+
+    @Test fun testSerialization() {
+        assertEquals(json, serialize(value))
+    }
+
+    @Test fun testDeserialization() {
+        assertEquals(value, deserialize(json))
+    }
+}
