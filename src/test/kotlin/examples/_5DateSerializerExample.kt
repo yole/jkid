3b3575f6d046fa95a4c7db823e904213e7c84497
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-06-04 11:00:09 +0200

    Minor: removed main from test

--- src/test/kotlin/examples/_5DateSerializerExample.kt
+++ src/test/kotlin/examples/_5DateSerializerExample.kt
@@ -4,7 +4,6 @@ import org.junit.Test
 import ru.yole.jkid.JsonSerializer
 import ru.yole.jkid.ValueSerializer
 import ru.yole.jkid.examples.jsonSerializerTest.testJsonSerializer
-import ru.yole.jkid.serialization.serialize
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -28,9 +27,4 @@ class DateSerializerTest {
             value = Person("Alice", SimpleDateFormat("dd-mm-yyyy").parse("13-02-1987")),
             json = """{"birthDate": "13-02-1987", "name": "Alice"}"""
     )
-}
-
-fun main(args: Array<String>) {
-    val birthDate = SimpleDateFormat("dd-mm-yyyy").parse("13-02-1987")
-    println(serialize(Person("Alice", birthDate)))
 }
