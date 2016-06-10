9c788f68947ef2ed28ef90ca2d66887af1da4eb6
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-06-10 12:24:43 +0300

    Removed simple value seed

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -18,7 +18,7 @@ internal fun <T : Any> KClass<T>.newInstance(): T {
     return noArgConstructor.call()
 }
 
-fun Type.asJavaClass(): Class<Any> = when(this) {
+fun Type.asJavaClass(): Class<Any> = when (this) {
     is Class<*> -> this as Class<Any>
     is ParameterizedType -> rawType as? Class<Any>
             ?: throw UnsupportedOperationException("Unknown type $this")
@@ -27,8 +27,13 @@ fun Type.asJavaClass(): Class<Any> = when(this) {
 
 fun <T> Iterable<T>.joinToStringBuilder(stringBuilder: StringBuilder, separator: CharSequence = ", ", prefix: CharSequence = "", postfix: CharSequence = "", limit: Int = -1, truncated: CharSequence = "...", callback: ((T) -> Unit)? = null): StringBuilder {
     return joinTo(stringBuilder, separator, prefix, postfix, limit, truncated) {
-        if (callback == null) return@joinTo  it.toString()
+        if (callback == null) return@joinTo it.toString()
         callback(it)
         ""
     }
+}
+
+fun Type.isPrimitive(): Boolean {
+    val cls = this as? Class<Any> ?: return false
+    return cls.kotlin.javaPrimitiveType != null || cls == String::class.java
 }
--- src/main/kotlin/deserialization/Deserializer.kt
+++ src/main/kotlin/deserialization/Deserializer.kt
@@ -1,6 +1,7 @@
 package ru.yole.jkid.deserialization
 
 import ru.yole.jkid.asJavaClass
+import ru.yole.jkid.isPrimitive
 import java.io.Reader
 import java.io.StringReader
 import java.lang.reflect.ParameterizedType
@@ -20,12 +21,12 @@ inline fun <reified T: Any> deserialize(json: Reader): T {
 
 fun <T: Any> deserialize(json: Reader, targetClass: KClass<T>): T {
     val seed = ObjectSeed(targetClass, ClassInfoCache())
-    val stack = Stack<CompositeSeed>()
+    val stack = Stack<Seed>()
     stack.push(seed)
 
     val callback = object : JsonParseCallback {
         override fun enterObject(propertyName: String) {
-            val compositeSeed = stack.peek().createCompositeSeed(propertyName)
+            val compositeSeed = stack.peek().createCompositeProperty(propertyName)
             stack.push(compositeSeed)
         }
 
@@ -42,81 +43,94 @@ fun <T: Any> deserialize(json: Reader, targetClass: KClass<T>): T {
         }
 
         override fun visitValue(propertyName: String, value: Any?) {
-            stack.peek().createSimpleSeed(propertyName, value)
+            stack.peek().setSimpleProperty(propertyName, value)
         }
     }
-    val parser = Parser(json, callback)
-    parser.parse()
+    Parser(json, callback).parse()
     return seed.spawn()
 }
 
+
 interface Seed {
+
+    val classInfoCache: ClassInfoCache
+
+    fun setSimpleProperty(propertyName: String, value: Any?)
+
+    fun createCompositeProperty(propertyName: String): Seed
+
     fun spawn(): Any?
 }
 
-class SimpleValueSeed(val value: Any?): Seed {
-    override fun spawn() = value
-}
+fun Seed.createSeedForType(paramType: Type): Seed {
+    val paramClass = paramType.asJavaClass()
 
-abstract class CompositeSeed(val classInfoCache: ClassInfoCache): Seed {
-    abstract fun createSimpleSeed(propertyName: String, value: Any?)
-    abstract fun createCompositeSeed(propertyName: String): CompositeSeed
+    if (Collection::class.java.isAssignableFrom(paramClass)) {
+        val parameterizedType = paramType as? ParameterizedType
+                ?: throw UnsupportedOperationException("Unsupported parameter type $this")
 
-    protected fun createSeedForType(paramType: Type): CompositeSeed {
-        val paramClass = paramType.asJavaClass()
-
-        if (Collection::class.java.isAssignableFrom(paramClass)) {
-            val parameterizedType = paramType as? ParameterizedType
-                    ?: throw UnsupportedOperationException(
-                        "Unsupported parameter type $this")
-
-            val elementType = parameterizedType.actualTypeArguments.single()
-            return CollectionSeed(elementType, classInfoCache)
+        val elementType = parameterizedType.actualTypeArguments.single()
+        if (elementType.isPrimitive()) {
+            return ValueCollectionSeed(classInfoCache)
         }
-        return ObjectSeed(paramClass.kotlin, classInfoCache)
+        return ObjectCollectionSeed(elementType, classInfoCache)
     }
+    return ObjectSeed(paramClass.kotlin, classInfoCache)
 }
+
 
 class ObjectSeed<out T: Any>(
         targetClass: KClass<T>,
-        classInfoCache: ClassInfoCache
-) : CompositeSeed(classInfoCache) {
+        override val classInfoCache: ClassInfoCache
+) : Seed {
 
-    private val classInfo = classInfoCache[targetClass]
+    private val classInfo: ClassInfo<T> = classInfoCache[targetClass]
 
     private val valueArguments = mutableMapOf<KParameter, Any?>()
-    private val seedArguments = mutableMapOf<KParameter, CompositeSeed>()
+    private val seedArguments = mutableMapOf<KParameter, Seed>()
 
-    override fun createSimpleSeed(propertyName: String, value: Any?) {
+    private val arguments: Map<KParameter, Any?>
+        get() = valueArguments + seedArguments.mapValues { it.value.spawn() }
+
+    override fun setSimpleProperty(propertyName: String, value: Any?) {
         val param = classInfo.getConstructorParameter(propertyName)
         valueArguments[param] = classInfo.deserializeConstructorArgument(param, value)
     }
 
-    override fun createCompositeSeed(propertyName: String): CompositeSeed {
+    override fun createCompositeProperty(propertyName: String): Seed {
         val param = classInfo.getConstructorParameter(propertyName)
         return createSeedForType(param.type.javaType).apply { seedArguments[param] = this }
     }
 
-    override fun spawn(): T {
-        val argumentValues = valueArguments + seedArguments.mapValues { it.value.spawn() }
-        return classInfo.newInstance(argumentValues)
-    }
+    override fun spawn(): T = classInfo.newInstance(arguments)
 }
 
-class CollectionSeed(
+class ObjectCollectionSeed(
         val elementType: Type,
-        classInfoCache: ClassInfoCache
-) : CompositeSeed(classInfoCache) {
-
+        override val classInfoCache: ClassInfoCache
+) : Seed {
     private val elements = mutableListOf<Seed>()
 
-    override fun createSimpleSeed(propertyName: String, value: Any?) {
-        val seed = SimpleValueSeed(value)
-        elements.add(seed)
+    override fun setSimpleProperty(propertyName: String, value: Any?) {
+        throw JKidException("Found primitive value in collection of object types")
     }
 
-    override fun createCompositeSeed(propertyName: String) =
-        createSeedForType(elementType).apply { elements.add(this) }
+    override fun createCompositeProperty(propertyName: String) =
+            createSeedForType(elementType).apply { elements.add(this) }
 
     override fun spawn(): Collection<*> = elements.map { it.spawn() }
+}
+
+class ValueCollectionSeed(override val classInfoCache: ClassInfoCache) : Seed {
+    private val elements = mutableListOf<Any?>()
+
+    override fun setSimpleProperty(propertyName: String, value: Any?) {
+        elements.add(value)
+    }
+
+    override fun createCompositeProperty(propertyName: String): Seed {
+        throw JKidException("Found object value in collection of primitive types")
+    }
+
+    override fun spawn() = elements
 }
