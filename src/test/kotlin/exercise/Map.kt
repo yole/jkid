21b8b7935d8eda335ee13bf08bc67754d554e819
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-06-14 15:25:31 +0300

    Renamed `MapSeedTest` to `MapTest`

--- src/test/kotlin/exercise/Map.kt
+++ src/test/kotlin/exercise/Map.kt
@@ -9,7 +9,7 @@ import kotlin.test.assertEquals
 data class BookStore(val bookPrice: Map<String, Double>)
 
 @Ignore
-class MapSeedTest {
+class MapTest {
     private val bookStore = BookStore(mapOf("Catch-22" to 10.92, "The Lord of the Rings" to 11.49))
     private val json = """{"bookPrice": {"Catch-22": 10.92, "The Lord of the Rings": 11.49}}"""
 
