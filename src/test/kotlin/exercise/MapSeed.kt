023cb52306a8e699afa2cc8352785adc7591d91f
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-06-14 15:00:00 +0300

    Added a test for map support

---
+++ src/test/kotlin/exercise/MapSeed.kt
@@ -0,0 +1,24 @@
+package exercise
+
+import org.junit.Ignore
+import org.junit.Test
+import ru.yole.jkid.deserialization.deserialize
+import ru.yole.jkid.serialization.serialize
+import kotlin.test.assertEquals
+
+data class BookStore(val bookPrice: Map<String, Double>)
+
+@Ignore
+class MapSeedTest {
+    private val bookStore = BookStore(mapOf("Catch-22" to 10.92, "The Lord of the Rings" to 11.49))
+    private val json = """{"bookPrice": {"Catch-22": 10.92, "The Lord of the Rings": 11.49}}"""
+
+    @Test fun testSerialization() {
+        println(serialize(bookStore))
+        assertEquals(json, serialize(bookStore))
+    }
+
+    @Test fun testDeserialization() {
+        assertEquals(bookStore, deserialize(json))
+    }
+}
