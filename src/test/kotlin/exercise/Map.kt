be6538ca0753b2d20e682b7e6eae8b2728789218
qpzm <lhm1442@gmail.com>
2020-11-22 16:13:12 +0900

    Add tests for a object-valued map

--- src/test/kotlin/exercise/Map.kt
+++ src/test/kotlin/exercise/Map.kt
@@ -7,11 +7,21 @@ import ru.yole.jkid.serialization.serialize
 import kotlin.test.assertEquals
 
 data class BookStore(val bookPrice: Map<String, Double>)
+data class Book(val title: String, val price: Double)
+data class TitleToBook(val books: Map<String, Book>)
 
 @Ignore
 class MapTest {
     private val bookStore = BookStore(mapOf("Catch-22" to 10.92, "The Lord of the Rings" to 11.49))
     private val json = """{"bookPrice": {"Catch-22": 10.92, "The Lord of the Rings": 11.49}}"""
+
+    private val titleToBook = TitleToBook(
+        mapOf(
+            "Catch-22" to Book("Catch-22", 10.92),
+            "The Lord of the Rings" to Book("The Lord of the Rings", 11.49)
+        )
+    )
+    private val titleToBookJson = """{"books": {"Catch-22": {"price": 10.92, "title": "Catch-22"}, "The Lord of the Rings": {"price": 11.49, "title": "The Lord of the Rings"}}}"""
 
     @Test fun testSerialization() {
         println(serialize(bookStore))
@@ -20,5 +30,13 @@ class MapTest {
 
     @Test fun testDeserialization() {
         assertEquals(bookStore, deserialize(json))
+    }
+
+    @Test fun testObjectMapSerialization() {
+        assertEquals(titleToBookJson, serialize(titleToBook))
+    }
+
+    @Test fun testObjectMapDeserialization() {
+        assertEquals(titleToBook, deserialize(titleToBookJson))
     }
 }
